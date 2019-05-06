44f89c43122964c4888eec0fbcb8314776be4dee
Vlad <vlad.kuzmichw@gmail.com>
2019-05-06 17:40:56 +0300

    add image support

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -16,6 +16,16 @@ public class Message {
     @JoinColumn(name = "user_id")
     private User author;
 
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    private String filename;
+
     public Message() {
     }
 
